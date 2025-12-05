2c7cead850216cce457138af2ddec72bb14b8262
AstralRando <aaronviney3@gmail.com>
2025-12-04 22:55:59 -0600

    renamed zones and added special picks

--- main.py
+++ main.py
@@ -1,91 +1,65 @@
-#   Sorry, little weirdo. Threats won't work on me. 
 #   I can't go to hell. I'm all out of vacation days :/
 
-totesInz1A = []
-totesInz2A = []
-circulating = [{"name": "pt001", "items": ['1A-101-A1']}]
+totesIni1 = []
+totesIni2 = []
+circulating = [{"name": "pt001", "items": ['i1-e3']}]
 addToZone = False
 zones = ["z1a", "z2A"]
 menuState = 0
 
 while True:
 
-        # Enter main zones
-        z1A = [ '1A-101-A1', '1A-101-A2', '1A-101-A3', '1A-101-A4', '1A-101-A5', '1A-101-A6',
-                '1A-101-B1', '1A-101-B2', '1A-101-B3', '1A-101-B4', '1A-101-B5', '1A-101-B6',
-                '1A-101-C1', '1A-101-C2', '1A-101-C3', '1A-101-C4', '1A-101-C5', '1A-101-C6',
-                '1A-101-D1', '1A-101-D2', '1A-101-D3', '1A-101-D4', '1A-101-D5', '1A-101-D6',
-                '1A-101-E1', '1A-101-E2', '1A-101-E3', '1A-101-E4', '1A-101-E5', '1A-101-E6',
+        # Zones Init
+        i1 = [ 'i1-a1', 'i1-a2', 'i1-a3', 'i1-a4', 'i1-a5', 'i1-a6',
+                'i1-b1', 'i1-b2', 'i1-b3', 'i1-b4', 'i1-b5', 'i1-b6',
+                'i1-c1', 'i1-c2', 'i1-c3', 'i1-c4', 'i1-c5', 'i1-c6',
+                'i1-d1', 'i1-d2', 'i1-d3', 'i1-d4', 'i1-d5', 'i1-d6',
+                'i1-e1', 'i1-e2', 'i1-e3', 'i1-e4', 'i1-e5', 'i1-e6',]
 
-                '1A-102-A1', '1A-102-A2', '1A-102-A3', '1A-102-A4', '1A-102-A5', '1A-102-A6',
-                '1A-102-B1', '1A-102-B2', '1A-102-B3', '1A-102-B4', '1A-102-B5', '1A-102-B6',
-                '1A-102-C1', '1A-102-C2', '1A-102-C3', '1A-102-C4', '1A-102-C5', '1A-102-C6',
-                '1A-102-D1', '1A-102-D2', '1A-102-D3', '1A-102-D4', '1A-102-D5', '1A-102-D6',
-                '1A-102-E1', '1A-102-E2', '1A-102-E3', '1A-102-E4', '1A-102-E5', '1A-102-E6',
+        i2 = [ 'i2-a1', 'i2-a2', 'i2-a3', 'i2-a4', 'i2-a5', 'i2-a6',
+                'i2-b1', 'i2-b2', 'i2-b3', 'i2-b4', 'i2-b5', 'i2-b6',
+                'i2-c1', 'i2-c2', 'i2-c3', 'i2-c4', 'i2-c5', 'i2-c6',
+                'i2-d1', 'i2-d2', 'i2-d3', 'i2-d4', 'i2-d5', 'i2-d6',
+                'i2-e1', 'i2-e2', 'i2-e3', 'i2-e4', 'i2-e5', 'i2-e6',]
 
-                '1A-103-A1', '1A-103-A2', '1A-103-A3', '1A-103-A4', '1A-103-A5', '1A-103-A6',
-                '1A-103-B1', '1A-103-B2', '1A-103-B3', '1A-103-B4', '1A-103-B5', '1A-103-B6',
-                '1A-103-C1', '1A-103-C2', '1A-103-C3', '1A-103-C4', '1A-103-C5', '1A-103-C6',
-                '1A-103-D1', '1A-103-D2', '1A-103-D3', '1A-103-D4', '1A-103-D5', '1A-103-D6',
-                '1A-103-E1', '1A-103-E2', '1A-103-E3', '1A-103-E4', '1A-103-E5', '1A-103-E6',
+        isp = [ 'isp-a1', 'isp-a2', 'isp-a3', 'isp-a4', 'isp-a5', 'isp-a6',
+                'isp-b1', 'isp-b2', 'isp-b3', 'isp-b4', 'isp-b5', 'isp-b6',
+                'isp-c1', 'isp-c2', 'isp-c3', 'isp-c4', 'isp-c5', 'isp-c6',
+                'isp-d1', 'isp-d2', 'isp-d3', 'isp-d4', 'isp-d5', 'isp-d6',
+                'isp-e1', 'isp-e2', 'isp-e3', 'isp-e4', 'isp-e5', 'isp-e6',]
 
-                '1A-104-A1', '1A-104-A2', '1A-104-A3', '1A-104-A4', '1A-104-A5', '1A-104-A6',
-                '1A-104-B1', '1A-104-B2', '1A-104-B3', '1A-104-B4', '1A-104-B5', '1A-104-B6',
-                '1A-104-C1', '1A-104-C2', '1A-104-C3', '1A-104-C4', '1A-104-C5', '1A-104-C6',
-                '1A-104-D1', '1A-104-D2', '1A-104-D3', '1A-104-D4', '1A-104-D5', '1A-104-D6',
-                '1A-104-E1', '1A-104-E2', '1A-104-E3', '1A-104-E4', '1A-104-E5', '1A-104-E6']
-
-        z2A = [ '2A-101-A1', '2A-101-A2', '2A-101-A3', '2A-101-A4', '2A-101-A5', '2A-101-A6',
-                '2A-101-B1', '2A-101-B2', '2A-101-B3', '2A-101-B4', '2A-101-B5', '2A-101-B6',
-                '2A-101-C1', '2A-101-C2', '2A-101-C3', '2A-101-C4', '2A-101-C5', '2A-101-C6',
-                '2A-101-D1', '2A-101-D2', '2A-101-D3', '2A-101-D4', '2A-101-D5', '2A-101-D6',
-                '2A-101-E1', '2A-101-E2', '2A-101-E3', '2A-101-E4', '2A-101-E5', '2A-101-E6',
-
-                '2A-102-A1', '2A-102-A2', '2A-102-A3', '2A-102-A4', '2A-102-A5', '2A-102-A6',
-                '2A-102-B1', '2A-102-B2', '2A-102-B3', '2A-102-B4', '2A-102-B5', '2A-102-B6',
-                '2A-102-C1', '2A-102-C2', '2A-102-C3', '2A-102-C4', '2A-102-C5', '2A-102-C6',
-                '2A-102-D1', '2A-102-D2', '2A-102-D3', '2A-102-D4', '2A-102-D5', '2A-102-D6',
-                '2A-102-E1', '2A-102-E2', '2A-102-E3', '2A-102-E4', '2A-102-E5', '2A-102-E6',
-
-                '2A-103-A1', '2A-103-A2', '2A-103-A3', '2A-103-A4', '2A-103-A5', '2A-103-A6',
-                '2A-103-B1', '2A-103-B2', '2A-103-B3', '2A-103-B4', '2A-103-B5', '2A-103-B6',
-                '2A-103-C1', '2A-103-C2', '2A-103-C3', '2A-103-C4', '2A-103-C5', '2A-103-C6',
-                '2A-103-D1', '2A-103-D2', '2A-103-D3', '2A-103-D4', '2A-103-D5', '2A-103-D6',
-                '2A-103-E1', '2A-103-E2', '2A-103-E3', '2A-103-E4', '2A-103-E5', '2A-103-E6',
-
-                '2A-104-A1', '2A-104-A2', '2A-104-A3', '2A-104-A4', '2A-104-A5', '2A-104-A6',
-                '2A-104-B1', '2A-104-B2', '2A-104-B3', '2A-104-B4', '2A-104-B5', '2A-104-B6',
-                '2A-104-C1', '2A-104-C2', '2A-104-C3', '2A-104-C4', '2A-104-C5', '2A-104-C6',
-                '2A-104-D1', '2A-104-D2', '2A-104-D3', '2A-104-D4', '2A-104-D5', '2A-104-D6',
-                '2A-104-E1', '2A-104-E2', '2A-104-E3', '2A-104-E4', '2A-104-E5', '2A-104-E6']
-
-        SpecialPick = []
-
-        # Checks for circulating totes that need picks in the zone
+        # Checks for circulating totes that need picks in zones
         if menuState == 11:
                 for tote in circulating:
                         for item in tote['items']:
-                                if item in z1A:
-                                        print("Item is in zone: 1A")
-                                        totesInz1A.append(circulating[0])
-                                        circulating.pop(0)
-                                        print(f'Totes in zone 1A: {totesInz1A}')
+                                if item in i1:
+                                        print("Item is in zone: i1")
+                                        totesIni1.append(circulating[0])
+                                        totesIni1.pop(0)
+                                        print(f'Totes in zone i1: {totesIni1}')
                                         print(f'Totes circulating: {circulating}')
                                         break
-                                if item in z2A:
-                                        print("Item is in zone: 2A")
-                                        totesInz1A.append(circulating[0])
-                                        circulating.pop(0)
-                                        print(f'Totes in zone 1A: {totesInz2A}')
+                                if item in i2:
+                                        print("Item is in zone: i2")
+                                        totesIni2.append(circulating[0])
+                                        totesIni1.pop(0)
+                                        print(f'Totes in zone i2: {totesIni2}')
                                         print(f'Totes circulating: {circulating}')
                                         break
 
 # MenuState System
-        menus = ['0: Zone Checker', '1: Tote Induction', '2: Zone Scanner (1A)', '11: Auto Zone Entry']
+        menus = ['0: Zone Checker', '1: Tote Induction', '2: Zone Scanner (i1)',
+                 '3: SKU Picking', '11: Auto Zone Entry']
         print('')
         for item in menus:
                 print(item)
         menuState = int(input("Select a menu: "))
+
+        if menuState == 0:
+                print("")
+                print(f'Circulating line: {circulating}')
+                print(f'In zone i1: {totesIni1}')
+                print(f'in zone i2: {totesIni2}')
 
         if menuState == 1:
                 print("Tote induction")
@@ -102,15 +76,22 @@ while True:
                 tote = {"name": toteName, "items": items}
                 circulating.append(tote)
 
-        if menuState == 0:
-                print("")
-                print(f'Circulating line: {circulating}')
-                print(f'In zone 1A: {totesInz1A}')
-                print(f'in zone 2A: {totesInz2A}')
         
         if menuState == 2:
                 enter_tote = input("Enter a tote: ")
                 for item in tote['items']:
-                        if item in z1A:
-                                z1A.append(enter_tote)
-                                circulating.pop()
+                        if item in i1:
+                                i1.append(enter_tote)
+                                circulating.pop(tote)
+        
+        if menuState == 3:
+                on_cart = []
+                zone = input("Zone ID: ")
+                cart = input("Cart ID: ")
+                if zone in zones:
+                        print(True)
+                while True:
+                        pick_tote = input("Enter Tote: ")
+                        if pick_tote == "":
+                                break
+                on_cart.append(pick_tote)
--- temp.py
+++ temp.py
@@ -1,17 +1,33 @@
-totes = []
-circulating = []
+import pygame
+import sys
 
+pygame.init()
 
-name = input("Name tote: ")
-items = []
+screen_width, screen_height = 800, 600
+screen = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption("Pygame Text Example")
 
-while True:
-    item = input("Add item (blank to stop): ")
-    if item == "":
-        break
-    items.append(item)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+BLUE = (0, 0, 255)
 
-tote = {"name": name, "items": items}
-totes.append(tote)
+font = pygame.font.SysFont('arial', 30)
 
-print(totes)
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    
+    screen.fill(BLACK)
+
+    text_surface = font.render("System for the thing test with GUI", True, BLUE)
+    
+    text_rect = text_surface.get_rect(center=(screen_width/2, screen_height/2))
+
+    screen.blit(text_surface, text_rect)
+
+    pygame.display.update()
+
+pygame.quit()
+sys.exit()
