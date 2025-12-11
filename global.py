e32e1594c6a428c307df2b3f2991ff5d60924149
AstralRando <aaronviney3@gmail.com>
2025-12-11 15:14:36 -0600

    New warehouse system in works using JSON for file handling

--- global.py
+++ global.py
@@ -1,1 +1,13 @@
-print("test")
+import json
+
+# Get data from JSON files
+with open("data.json", "r") as zone:
+    jsondata = zone.read()
+    data = json.loads(jsondata)
+
+with open("totes.json", "r") as totes:
+    totedata = totes.read()
+    totes = json.loads(totedata)
+
+for item in totes:
+    print(item)
