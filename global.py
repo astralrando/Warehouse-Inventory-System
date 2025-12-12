1e31eaf019c860e783067931e7c5e118d3eabedb
AstralRando <aaronviney3@gmail.com>
2025-12-11 23:13:05 -0600

    uh i did a lot

--- global.py
+++ global.py
@@ -1,38 +1,67 @@
+#   I can't go to hell. I'm all out of vacation days :/
+
+#TODO
+#Induction \/
+#Tote eraser \/
+#Picking
+#Replenishment
+#Zone entry
+#Zone scanners
+#Special pick inplementation
+#Enter task
+
 import json
 
 # Initial variables
-menustates = ["1: System Check", "2: Induction"]
+menustates = ["1: System Check", "2: Induction", "3: Tote eraser"]
 menustate = 0
 
 # Get data from JSON files
 with open("data.json", "r") as zone:
     jsondata = zone.read()
-    data = json.loads(jsondata)
+    zones = json.loads(jsondata)
 
 with open("totes.json", "r") as totes:
     totedata = totes.read()
     totes = json.loads(totedata)
 
-for item in menustates:
-    print(item)
-menustate = int(input("Enter Menustate: "))
+print(totes)
 
-if menustate == 1:
-
-    print("Totes: ")
-    for item in totes:
+while True:
+    for item in menustates:
         print(item)
-    print("Zones: ")
-    for item in data:
-        print(item)
+    menustate = int(input("Enter Menustate: "))
 
-# Induction
-if menustate == 2:
-    needs = []
-    toteName = input("Enter tote: ")
-    while True:
-        need = input("Input tote needs (leave blank to exit): ")
-        if need == "":
-            break
-        needs.append(need)
-    tote = {"name": toteName, "inTote": [], "needs": needs}
+    # System Check
+    if menustate == 1:
+
+        print(zones)
+        print(totes)
+        menustate = 0
+
+    # Induction
+    if menustate == 2:
+        needs = []
+        toteName = input("Enter tote: ")
+        while True:
+            need = input("Input tote needs (leave blank to exit): ")
+            if need == "":
+                break
+            needs.append(need)
+        tote = {"name": toteName, "inTote": [], "needs": needs}
+        totes.append(tote)
+        print(totes)
+        with open("totes.json", "w") as totedata:
+            totepack = json.dumps(totes)
+            totedata.write(totepack)
+        menustate = 0
+    
+    # Tote Eraser
+    if menustate == 3:
+        erasetote = input("Input tote to remove: ")
+        for tote in reversed(range(len(totes))):
+            if totes[tote]['name'] == erasetote:
+                del totes[tote]
+        with open("totes.json", "w") as totedata:
+            totepack = json.dumps(totes)
+            totedata.write(totepack)
