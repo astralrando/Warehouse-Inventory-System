816bef37603d520ace688f92c0fd5c264f38c808
AstralRando <aaronviney3@gmail.com>
2025-12-22 16:15:05 -0600

    ебат

---
+++ Main.java
@@ -0,0 +1,12 @@
+public class Main{
+    public static void main(String[] args){
+        boolean isloggedin = true;
+        boolean isadmin = true;
+        if (isloggedin && isadmin){
+            System.out.println(true);
+        }
+        else{
+            System.out.println(false);
+        }
+    }
+}
