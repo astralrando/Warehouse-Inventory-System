37afd065771ecb95dbf2f594a88d5989b9da03cd
AstralRando <aaronviney3@gmail.com>
2025-12-31 22:06:17 -0600

    Starting Java version

--- Java/Main.java
+++ Java/Main.java
@@ -2,13 +2,6 @@ import java.util.Scanner;
 
 public class Main{
     public static void main(String[] args){
-        boolean isloggedin = true;
-        boolean isadmin = true;
-        if (isloggedin && isadmin){
-            System.out.println(true);
-        }
-        else{
-            System.out.println(false);
-        }
+        boolean isDebug = false;
     }
 }
